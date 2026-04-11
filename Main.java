2cda9c44116b81562de24d4f8e6bff4969215523
Hriday-sarkar <connecthridaysarkar@gmail.com>
2026-04-11 17:32:28 +0530

    TableManager connected to the main

--- Main.java
+++ Main.java
@@ -22,22 +22,27 @@ choice = input.nextInt();
 switch(choice){
 case 1:
 System.out.println("So you want to create a Table");
+TableManager.createTable();
 break;
 
 case 2:
 System.out.println("Insert Table selected");
+TableManager.insertRecord();
 break;
 
 case 3:
 System.out.println("View record selected");
+TableManager.viewRecords();
 break;
 
 case 4:
 System.out.println("Search record selected");
+TableManager.searchRecord();
 break;
 
 case 5:
 System.out.println("Delete record selected");
+TableManager.deleteRecord();
 break;
 
 case 6:
---
+++ TableManager.java
@@ -0,0 +1,18 @@
+public class TableManager{
+
+static void createTable(){
+System.out.println("Creating table............");
+}
+static void insertRecord(){
+System.out.println("Inserting Record............");
+}
+static void viewRecords(){
+System.out.println("Viewing Records.............");
+}
+static void searchRecord(){
+System.out.println("Searching Records............");
+}
+static void deleteRecord(){
+System.out.println("Deleting Records............");
+}
+}
