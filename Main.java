0f4707ef50510c213e26cc4d4e27090b3203dd45
Hriday-sarkar <connecthridaysarkar@gmail.com>
2026-04-11 12:49:04 +0530

    coded basic menu with no logic

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main{
+public static void main(String[] args){
+System.out.println(
+"HELLO BOSS... WHAT'S ON YOUR MIND TODAY...:\n"
++"1.Create Table\n"
++"2.Insert Table\n"
++"3.View Record\n"
++"4.Search Record\n"
++"5.Delete Record\n"
++"6.EXIT");
+}
+}
