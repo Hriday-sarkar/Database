bbfd694f32971b6b98f72e77e4e815b256445d5a
Hriday-sarkar <connecthridaysarkar@gmail.com>
2026-04-11 14:47:47 +0530

    fully coded menu inside main

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
 public class Main{
 public static void main(String[] args){
+
+Scanner hr= new Scanner(System.in);
+int choice;
+
+do{
 System.out.println(
 "HELLO BOSS... WHAT'S ON YOUR MIND TODAY...:\n"
 +"1.Create Table\n"
@@ -8,5 +14,39 @@ System.out.println(
 +"4.Search Record\n"
 +"5.Delete Record\n"
 +"6.EXIT");
+
+
+choice = hr.nextInt();
+
+
+switch(choice){
+case 1:
+System.out.println("So you want to create a Table");
+break;
+
+case 2:
+System.out.println("Insert Table selected");
+break;
+
+case 3:
+System.out.println("View record selected");
+break;
+
+case 4:
+System.out.println("Search record selected");
+break;
+
+case 5:
+System.out.println("Delete record selected");
+break;
+
+case 6:
+System.out.println("SEE YOU LATER BYE");
+break;
+
+default:
+System.out.println("ARE YOU KIDDING... SELECT A REAL OPTION...");
+}
+}while(choice!=6);
 }
 }
